edb16fa29abe92c5bbe259028916dcc4afabff72
baruchrj1 <baruchrj2003@gmail.com>
2026-01-11 21:21:13 -0300

    Fix cookie name for production environment

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -40,7 +40,7 @@ export const authOptions: NextAuthOptions = {
     // Configure session cookie to expire when browser closes
     cookies: {
         sessionToken: {
-            name: `next-auth.session-token`,
+            name: `${process.env.NODE_ENV === "production" ? "__Secure-" : ""}next-auth.session-token`,
             options: {
                 httpOnly: true,
                 sameSite: "lax",
